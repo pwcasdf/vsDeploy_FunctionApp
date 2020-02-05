b99bef177dde168241cb6b060d3bfd78a06675bd
pwcasdf <pwcasdf@gmail.com>
2020-02-05 10:13:49 +0900

    service bus trigger

--- OsangHC_FunctionApp/Function1.cs
+++ OsangHC_FunctionApp/Function1.cs
@@ -4,18 +4,30 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.Azure.Devices;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Devices;
 
 namespace OsangHC_FunctionApp
 {
     public static class Function1
     {
         [FunctionName("c2dFunctionApp")]
-        public static void Run([ServiceBusTrigger("c2dqueue", Connection = "connectionString")]string myQueueItem, ILogger log)
+        public static async void Run([ServiceBusTrigger("c2dqueue", Connection = "connectionString")]string myQueueItem, ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            string connectionString = "Connection_String";
+
+            var jsonInput = (JObject)JsonConvert.DeserializeObject(myQueueItem);
+            var deviceName=((JValue)jsonInput["DeviceName"]).ToString();
+            var temperature = ((JValue)jsonInput["temperature"]).ToString();
+            dynamic jsonOutput = new JObject();
+            jsonOutput.Add("DeviceName", deviceName);
+            jsonOutput.Add("temperature", temperature);
+            string jsonOutputString = jsonOutput.ToString(Formatting.None);
+
+            ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            var serviceMessage = new Microsoft.Azure.Devices.Message(Encoding.ASCII.GetBytes(jsonOutputString));
+            await serviceClient.SendAsync(deviceName, serviceMessage);
         }
     }
 }
